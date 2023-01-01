996fdf0669c83b1e722f58908d1cca7a9f16578d
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-01 23:58:27 +0600

    number and post added

--- test.js
+++ test.js
@@ -15,27 +15,3 @@ function userNameValidation(usernameInput) {
     username.style.borderColor = defaultInput;
   }
 }
-
-function passwordValidation(passwordInput) {
-  var password = document.getElementById("password");
-  var issueArr = [];
-  if (!/^.{7,15}$/.test(passwordInput)) {
-    issueArr.push("Password must be between 7-15 characters.");
-  }
-  if (!/\d/.test(passwordInput)) {
-    issueArr.push("Must contain at least one number.");
-  }
-  if (!/[a-z]/.test(passwordInput)) {
-    issueArr.push("Must contain a lowercase letter.");
-  }
-  if (!/[A-Z]/.test(passwordInput)) {
-    issueArr.push("Must contain an uppercase letter.");
-  }
-  if (issueArr.length > 0) {
-    password.setCustomValidity(issueArr.join("\n"));
-    password.style.borderColor = alertRedInput;
-  } else {
-    password.setCustomValidity("");
-    password.style.borderColor = defaultInput;
-  }
-}
