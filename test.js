5bdef8fc8a2026520ca8c9e5800dffe63c326be7
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-02 22:23:40 +0600

    modal also working

--- test.js
+++ test.js
@@ -16,6 +16,22 @@ function userNameValidation(usernameInput) {
   }
 }
 
+
+function emailValidation(emailInput) {
+  var email = document.getElementById("email");
+  var issueArr = [];
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)) {
+    issueArr.push("Enter email format like ab@cd.com");
+  }
+  if (issueArr.length > 0) {
+    email.setCustomValidity(issueArr.join("\n"));
+    email.style.borderColor = alertRedInput;
+  } else {
+    email.setCustomValidity("");
+    email.style.borderColor = defaultInput;
+  }
+}
+
 function postValidation(postInput) {
   var post = document.getElementById("post");
   var issueArr = [];
@@ -53,4 +69,36 @@ function phoneValidation(phoneInput) {
     phone.setCustomValidity("");
     phone.style.borderColor = defaultInput;
   }
+}
+
+
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onsubmit = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function test() {
+  console.log('submited')
 }
