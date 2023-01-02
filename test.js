5688744be91b748f3ce0dfc23da71babd7a3070f
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-02 14:42:40 +0600

    phone number validation also done

--- test.js
+++ test.js
@@ -16,37 +16,13 @@ function userNameValidation(usernameInput) {
   }
 }
 
-// function passwordValidation(passwordInput) {
-//   var password = document.getElementById("password");
-//   var issueArr = [];
-//   if (!/^.{7,15}$/.test(passwordInput)) {
-//     issueArr.push("Password must be between 7-15 characters.");
-//   }
-//   if (!/\d/.test(passwordInput)) {
-//     issueArr.push("Must contain at least one number.");
-//   }
-//   if (!/[a-z]/.test(passwordInput)) {
-//     issueArr.push("Must contain a lowercase letter.");
-//   }
-//   if (!/[A-Z]/.test(passwordInput)) {
-//     issueArr.push("Must contain an uppercase letter.");
-//   }
-//   if (issueArr.length > 0) {
-//     password.setCustomValidity(issueArr.join("\n"));
-//     password.style.borderColor = alertRedInput;
-//   } else {
-//     password.setCustomValidity("");
-//     password.style.borderColor = defaultInput;
-//   }
-// }
-
 function postValidation(postInput) {
   var post = document.getElementById("post");
   var issueArr = [];
   if (!/^.{4}$/.test(postInput)) {
     issueArr.push("Post Must be in 4 character");
   }
-  if (!/^[0-9]/.test(postInput)) {
+  if (/[^0-9]/.test(postInput)) {
     issueArr.push("Post Must in number");
   }
   if (issueArr.length > 0) {
@@ -55,5 +31,26 @@ function postValidation(postInput) {
   } else {
     post.setCustomValidity("");
     post.style.borderColor = defaultInput;
+  }
+}
+
+function phoneValidation(phoneInput) {
+  var phone = document.getElementById("phone");
+  var issueArr = [];
+  if (!/^.{11}$/.test(phoneInput)) {
+    issueArr.push("Phone Must be in 11 character");
+  }
+  if (!/^01/.test(phoneInput)) {
+    issueArr.push("Should Start with 01");
+  }
+  if (/[^0-9]/.test(phoneInput)) {
+    issueArr.push("Character Must be number");
+  }
+  if (issueArr.length > 0) {
+    phone.setCustomValidity(issueArr.join("\n"));
+    phone.style.borderColor = alertRedInput;
+  } else {
+    phone.setCustomValidity("");
+    phone.style.borderColor = defaultInput;
   }
 }
