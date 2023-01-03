efc2e6aa5b614bd89e2f77285d7164602e9b8fe6
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-03 17:10:32 +0600

    email regax added

--- test.js
+++ test.js
@@ -71,34 +71,9 @@ function phoneValidation(phoneInput) {
   }
 }
 
-
-// modal
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onsubmit = function () {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-function test() {
-  console.log('submited')
+  let infoTest = document.getElementById('testInfo');
+  infoTest.innerText = 'You have tested'
 }
